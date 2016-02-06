b862ad68049bb33fc27964efc74314060a4c8a32
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-02-05 21:39:40 -0800

    Finish 21

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import API_KEY from '../secret.js'
+import API_KEY from '../secret.js';
 
-//Create a new component. This should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      //this.setState({ videos: videos }); //convert to ES6 below
+      this.setState({ videos });
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
-// This this component's HTML and put it in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
