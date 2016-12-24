796efdec6235226acdd59fd7972110b9cb4011b7
Joshua Henson <joshuahenson@users.noreply.github.com>
2016-12-23 19:53:05 -0800

    finish 6

--- js30/06 - Type Ahead/js/index.js
+++ js30/06 - Type Ahead/js/index.js
@@ -1,1 +1,39 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+fetch(endpoint)
+  .then(blob => blob.json())
+  .then(data => cities.push(...data));
+
+function findMatches(word, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(word, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(x) {
+  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
+function displayMatches() {
+  const places = findMatches(this.value, cities);
+  const html = places.map(place => {
+    const regex = new RegExp(this.value, 'gi');
+    const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+    return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
+  }).join('');
+  console.log(html);
+  suggestions.innerHTML = html;
+}
+
+const searchField = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+searchField.addEventListener('keyup', displayMatches);
