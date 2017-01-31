730927623d3592b63d3387abbac9be47f8b3112d
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-01-30 21:26:03 -0800

    Finish 15

--- js30/15 - LocalStorage/js/index.js
+++ js30/15 - LocalStorage/js/index.js
@@ -1,3 +1,42 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];
+const addItems = document.querySelector('.add-items'); // form
+const itemsList = document.querySelector('.plates'); // ul
+const items = JSON.parse(localStorage.getItem('items')) || [];
+
+function populateList(items = [], list) {
+ // don't use innerHTML for real stuff
+  list.innerHTML = items.map((item, i) => {
+    return `
+        <li>
+          <input type="checkbox" data-index=${i} id="item${i}" ${item.done ? 'checked' : ''} />
+          <label for="item${i}">${item.text}</label>
+        </li>
+      `;
+  }).join('');
+}
+
+function toggleDone(e) {
+  if (!e.target.matches('input')) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+function addItem(e) {
+  e.preventDefault();
+  const text = this.querySelector('[name=item]').value;
+  const item = {
+    text,
+    done: false
+  };
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
+}
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
