1f7969d00af2356071cae35c8815f70d0d5ada45
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-02-01 21:17:55 -0800

    Finished 17

--- js30/17 - Sort Without Articles/js/index.js
+++ js30/17 - Sort Without Articles/js/index.js
@@ -1,1 +1,15 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+function stripArt(name) {
+  return name.replace(/^(a |the |an )/i, '').trim();
+}
+const sorted = bands.sort((a, b) => stripArt(a) > stripArt(b) ? 1 : -1);
+
+function addItem(item) {
+  const li = document.createElement('li');
+  const itemText = document.createTextNode(item);
+  li.appendChild(itemText);
+  return li;
+}
+const bandList = document.querySelector('#bands');
+sorted.forEach(band => bandList.appendChild(addItem(band)));
