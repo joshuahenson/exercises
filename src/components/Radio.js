43aa227628448b2530a39d7f9936741225da1506
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-04-22 19:10:32 -0700

    Bumped react to 15.5

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './Radio.css';
 
 const Radio = ({ name, id, label, value, checked, clickHandler }) => {
