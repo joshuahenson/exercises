d30af99989b2a44d5fe4875d9791819ce344df93
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-04-24 18:25:34 -0700

    Prevent duplicates from being added

--- src/reducers/oilsReducer.js
+++ src/reducers/oilsReducer.js
@@ -1,6 +1,9 @@
 export default(state = [], action) => {
   switch (action.type) {
     case 'ADD_OIL':
+      if (state.some(oil => oil.oilId === action.oil.oilId)) {
+        return state;
+      }
       return [...state, { ...action.oil, value: 0 }];
     default:
       return state;
