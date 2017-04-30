bee587c4bf21c8b20cd4f0fc93a731094e410173
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-04-30 11:57:08 -0700

    Calculate remaining ingredients

--- src/components/Oils.js
+++ src/components/Oils.js
@@ -5,28 +5,38 @@ import InputUnit from './InputUnit';
 import { setOilValue, removeOil } from '../actions';
 import './Oils.css';
 
-const Oils = ({ oils, unit, byPercent, setOilValue, removeOil }) => {
+const Oils = ({ oils, unit, byPercent, setOilValue, removeOil, oilWeight }) => {
+  const totalIngredients = oils.reduce((a, b) => (a + parseInt(b.value, 10)), 0);
   return (
-    <ul className="oils">
-      {
-        oils.map((oil) => {
-          return (
-            <li key={oil.oil}>
-              <div className="i-b">
-                <InputUnit
-                  id={`${oil.oilId}_oil_weight`} label={oil.oil} min="0" value={oil.value}
-                  unit={byPercent ? '%' : unit}
-                  onChange={e => setOilValue(e.target.value, oil.oilId)}
-                />
-              </div>
-              <button className="remove" type="button" onClick={() => removeOil(oil.oilId)}>
-                X
-              </button>
-            </li>
-          );
-        })
-      }
-    </ul>
+    <div>
+      <ul className="oils">
+        {
+          oils.map((oil) => {
+            return (
+              <li key={oil.oil}>
+                <div className="i-b">
+                  <InputUnit
+                    id={`${oil.oilId}_oil_weight`} label={oil.oil} min="0" value={oil.value}
+                    unit={byPercent ? '%' : unit}
+                    onChange={e => setOilValue(e.target.value, oil.oilId)}
+                  />
+                </div>
+                <button className="remove" type="button" onClick={() => removeOil(oil.oilId)}>
+                  X
+                </button>
+              </li>
+            );
+          })
+        }
+      </ul>
+      <p>
+        {
+          byPercent ?
+          `${100 - totalIngredients}% remaining` :
+          `${oilWeight - totalIngredients}${unit} remaining`
+        }
+      </p>
+    </div>
   );
 };
 
@@ -35,14 +45,16 @@ Oils.propTypes = {
   unit: PropTypes.string,
   byPercent: PropTypes.bool,
   setOilValue: PropTypes.func,
-  removeOil: PropTypes.func
+  removeOil: PropTypes.func,
+  oilWeight: PropTypes.string
 };
 
 function mapStateToProps(state) {
   return {
     oils: state.oils,
     unit: state.unit,
-    byPercent: state.byPercent
+    byPercent: state.byPercent,
+    oilWeight: state.oilWeight
   };
 }
 
