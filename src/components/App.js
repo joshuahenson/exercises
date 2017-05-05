f5666b0d520a78754c6e43b1176b00740751c97d
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-05-04 19:05:50 -0700

    Initial React Router

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Calculator from './Calculator';
 
+// TODO: Replace test with actual 404
 const App = () => {
   return (
-    <Calculator />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Calculator} />
+        <Route component={Calculator} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
