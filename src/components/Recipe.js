754a33ab222a07c1dd62f4c8181f752605cf4bcc
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-05-06 11:19:49 -0700

    Initial styling

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -2,38 +2,40 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import round from 'lodash/round';
-// import './Recipe.css';
+import './Recipe.css';
 
 // TODO: redirect to calculator if no oils
 
 const Recipe = ({ oils, unit, oilWeight, waterRatio, soapType }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Ingredient</th>
-          <th>Quantity</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Water</td>
-          <td>{`${round(oilWeight * waterRatio / 100, 2)} ${unit}`}</td>
-        </tr>
-        <tr>
-          <td>{soapType === 'naoh' ? 'Sodium Hydroxide (NaOH)' : 'Potassium Hydroxide (KOH)'}</td>
-          <td>{`${round(oils.reduce((a, b) => (a + b.value * b[soapType]), 0), 2)} ${unit}`}</td>
-        </tr>
-        {oils.map((oil) => {
-          return (
-            <tr key={oil.oilId}>
-              <td>{oil.oil}</td>
-              <td>{`${oil.value} ${unit}`}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <div className="recipe">
+      <table>
+        <thead>
+          <tr>
+            <th className="ingredient">Ingredients</th>
+            <th>Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Water</td>
+            <td>{`${round(oilWeight * waterRatio / 100, 2)} ${unit}`}</td>
+          </tr>
+          <tr>
+            <td>{soapType === 'naoh' ? 'Sodium Hydroxide (NaOH)' : 'Potassium Hydroxide (KOH)'}</td>
+            <td>{`${round(oils.reduce((a, b) => (a + b.value * b[soapType]), 0), 2)} ${unit}`}</td>
+          </tr>
+          {oils.map((oil) => {
+            return (
+              <tr key={oil.oilId}>
+                <td>{oil.oil}</td>
+                <td>{`${oil.value} ${unit}`}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
