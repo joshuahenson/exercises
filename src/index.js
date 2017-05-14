f3871f473d97c326fcdbd2c62d67cd80f7da7e06
Joshua Henson <joshuahenson@users.noreply.github.com>
2017-05-13 19:07:58 -0700

    Added redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
+/* global window document */
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import App from './components/App';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
   <Provider
